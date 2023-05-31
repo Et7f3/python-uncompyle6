abb79ebd26d9cac3d0c3429cd6172c40b5f13482
Et7f3 <cadeaudeelie@gmail.com>
2023-05-31 12:59:43 +0000

    test: if/else inside loop become nested

---
+++ test/simple_source/looping/01_if_else_loop.py
@@ -0,0 +1,10 @@
+for cmd in [ 'SEND' ]:
+    if cmd == 'EXPECT':
+        try:
+            print("invalid")
+        except:
+            print("invalid")
+    if cmd == 'SEND':
+        print("valid")
+    else:
+        print("invalid")
